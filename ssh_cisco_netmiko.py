fcda1dd7162020f94d8084d516f5b319d3d3480c
watyanabe164 <50786237+watyanabe164@users.noreply.github.com>
2020-07-01 17:55:53 +0900

    first commit

---
+++ ssh_cisco_netmiko.py
@@ -0,0 +1,17 @@
+﻿import netmiko
+
+#デバイス情報の記載
+device = {
+    'device_type':'cisco_ios',
+    'ip':'172.19.0.2',
+    'username':'admin',
+    'password':'nM6yPmhL',
+    'secret':'mainte',
+}
+
+#コマンドの実行
+with netmiko.ConnectHandler(**device) as con:
+    con.enable()
+    output = con.send_command("show run")
+    print(output)
+    con.disconnect()
---
+++ telnet_cisco.py
@@ -0,0 +1,18 @@
+﻿import getpass
+import telnetlib
+
+HOST = '172.19.0.2'
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+if password:
+    tn.read_until(b'Password: ')
+    tn.write(password.encode('ascii') + b'\n')
+
+# Ciscoコマンド発行
+tn.write(b'show clock' + b'\n')
+tn.write(b'exit' + b'\n')
+
+# read_until以降のテキストを出力
+print(tn.read_all().decode('ascii'))
